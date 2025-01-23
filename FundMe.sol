7ac2b38fb7f43e7303d09fd7fd837147d8c980dc
space_lumps <165586177+space-lumps@users.noreply.github.com>
2025-01-23 18:28:20 -0500

    Update FundMe.sol
    accidentally pasted code from PriceConverter.sol in last commit

--- FundMe.sol
+++ FundMe.sol
@@ -1,28 +1,103 @@
 // SPDX-License-Identifier: MIT
 
+//get funds from users
+//withdraw funds
+// set a minimum funding value in usd
+
 pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; 
+import {PriceConverter} from "PriceConverter.sol";
 
-library PriceConverter {
-    function getPrice() internal view returns (uint256){
-        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI-- imported from github interface
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 answer,,,) = priceFeed.latestRoundData();
-        // returns price of ETH in terms of USD with 8 decimal places
-        return uint256(answer * 1e10);
+// constant and immutable keywords are stored directly in bytecode of contract instead of in storage
+// immutable allows values to be set at runtime/deployment time
+// constant allows values to be set at compile time
+
+error NotOwner();
+error CallFailed();
+error SendFailed();
+
+// 874483 gas
+// 854532 gas
+// 854520 gas
+contract FundMe {
+
+    using PriceConverter for uint256;
+
+    // USE ALL CAPS FOR VARIABLE NAME for constants 
+    uint256 public constant MINIMUM_USD = 5e18;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    // use i_ prefix for immutable variables
+    address public  immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
     }
 
-    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
-        // 1 ETH price? 
-        // 3000_000000000000000000
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+    function fund() public payable {
+
+        // allow users to send $
+        // have a minimum $ sent
+        // how do we send ETH to this contract?
+        // make contract payable
+       
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 8
+        
+
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function getVersion() internal view returns (uint256) {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    function withdraw() public onlyOwner {
+        // using for loop
+        // [1,2,3,4]
+        //  0,1,2,3
+            // for(starting index, ending index, step amount );
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex ++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset the array
+        funders = new address[](0);
+       
+        // withdraw the funds
+
+        // transfer
+        // msg.sender is an address
+        // payable(msg.sender) = pyaable address;
+        payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        //require(sendSuccess, "Send failed"); /*add require statement to make sure tx reverts if it fails*/  
+        if(sendSuccess = false) { revert SendFailed(); }
+        
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this). balance}("");
+        // require(callSuccess, "Call failed");
+        if(callSuccess = false) { revert CallFailed(); }
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "Sender is not owner");
+        //custom errors are more gas efficient:
+        if(msg.sender != i_owner) { revert NotOwner(); }
+        _;
+    }
+
+    // what happens if someone sends this contract ETH without calling FundMe?
+    
+    // receive()
+    // fallback()
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
